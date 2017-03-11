b6c8b843eba005a005c0416d4333247f07f29d31
TseHang <a54383813@gmail.com>
2017-03-11 18:58:34 +0800

    use 'markdown-it-calendar'

--- public/calendar/calendar.js
+++ public/calendar/calendar.js
@@ -24,22 +24,25 @@ const createCalendarHTML = (data) => {
 
   const firstDay = iterDate.getDay()
   const calendar = document.createElement('table')
+  const th = document.createElement('th')
   let tr, td
-  calendar.className = 'calendar'
+  calendar.className = 'calendar table table-responsive container-fluid'
   calendar.setAttribute('year', year)
   calendar.setAttribute('month', month)
 
   // init title
   tr = calendar.insertRow()
-  td = tr.insertCell()
-  td.colSpan = '7'
-  td.innerHTML = year + '-' + months[parseInt(month) - 1]
+  tr.appendChild(th)
+  th.colSpan = '7'
+  th.className = 'calendar-title'
+  th.innerHTML = months[parseInt(month) - 1] + '<span class="calendar-title-year"> ' + year + '</span>'
 
   // week name row
   tr = calendar.insertRow()
   for (let i = 0; i < 7; ++i) {
     td = tr.insertCell()
     td.innerHTML = weeks[i]
+    td.className = 'calendar-week'
   }
 
   // blank days
@@ -53,30 +56,30 @@ const createCalendarHTML = (data) => {
   // general days
   for (let i = firstDay; i < daysNum + firstDay; ++i) {
     const date = document.createElement('p');
-    const tag = document.createElement('p');
+    const tag = document.createElement('div');
+    const event = document.createElement('p');
     const today = i - firstDay + 1
 
     if (i % 7 === 0) {
       tr = calendar.insertRow()
     }
     td = tr.insertCell()
-    td.className = 'day'
+    td.className = 'calendar-day'
     iterDate.setDate(today)
 
     date.innerHTML = today
-    date.className = 'date'
-    date.id = `date_${year}_${month}_${today}`
+    date.className = 'calendar-date'
     td.appendChild(date)
 
     if (data.Content[iterDate]) {
-      data.Content[iterDate].forEach((e) => {
+      data.Content[iterDate].forEach((e, index) => {
         tag.innerHTML += e.title + ':' + e.description + '<br>'
         tag.className = 'tag'
-        tag.id = `tag_${year}_${month}_${today}`
         td.appendChild(tag)
       })
     }
   }
+
 
   // blank days
   for (let i = daysNum + firstDay; i % 7 !== 0; ++i) {
